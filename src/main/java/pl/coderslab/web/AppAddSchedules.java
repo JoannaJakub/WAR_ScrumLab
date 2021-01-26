9f4e594d32a50c68e1b54228fa1bb73a43349d15
Michał Kępiński <kepinski.poznan@gmail.com>
2021-01-26 15:36:55 +0100

    AppAddSchedule

---
+++ src/main/java/pl/coderslab/web/AppAddSchedules.java
@@ -0,0 +1,21 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/app/plan/add")
+public class AppAddSchedules extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        getServletContext().getRequestDispatcher("/appAddSchedules.jsp").forward(request, response);
+    }
+}
