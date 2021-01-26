3911a2fd718dd262282d0feb591427cc118bd989
Michał Kępiński <kepinski.poznan@gmail.com>
2021-01-26 14:40:32 +0100

    servlety i pliki jsp od Kontakt i O aplikacji

---
+++ src/main/java/pl/coderslab/web/About.java
@@ -0,0 +1,21 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/About")
+public class About extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        getServletContext().getRequestDispatcher("/about.jsp").forward(request, response);
+    }
+}
