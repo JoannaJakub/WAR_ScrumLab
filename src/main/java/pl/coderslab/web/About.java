dace8404a267dec4c7545e8268a8ca07042d9fbc
Michał Kępiński <kepinski.poznan@gmail.com>
2021-01-26 15:10:54 +0100

    komentarze usunięte, servlety podpięte #u mnie działa

--- src/main/java/pl/coderslab/web/About.java
+++ src/main/java/pl/coderslab/web/About.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/About")
+@WebServlet("/about")
 public class About extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
--- src/main/webapp/contact.jsp
+++ src/main/webapp/contact.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: michalkepinski
-  Date: 26/01/2021
-  Time: 13:18
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
