f938e2d0a5e9370e597fd2b1f778d7d395ce4e24
Michał Kępiński <kepinski.poznan@gmail.com>
2021-01-27 13:43:21 +0100

    Dodalem obsluge dodawania planu, usunąłem niepotrzebne komentarze z pliku jsp

--- src/main/java/pl/coderslab/web/AppAddSchedules.java
+++ src/main/java/pl/coderslab/web/AppAddSchedules.java
@@ -1,21 +1,41 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.model.Plan;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @WebServlet ("/app/plan/add")
 public class AppAddSchedules extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        setUpPlan(request, response);
+        response.sendRedirect(request.getContextPath() + "/app/plan/list");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         getServletContext().getRequestDispatcher("/appAddSchedules.jsp").forward(request, response);
+    }
+
+    public void setUpPlan(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String name = request.getParameter("planName");
+        String description = request.getParameter("planDesc");
+        Calendar cal = Calendar.getInstance();
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(cal.getTime());
+        int adminId = (int) session.getAttribute("id");
+        Plan plan = new Plan(0, name, description, adminId);
+        plan.setCreated(date);
+        PlanDao planDao = new PlanDao();
+        planDao.create(plan);
     }
 }
