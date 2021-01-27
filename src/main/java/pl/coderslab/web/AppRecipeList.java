37409e1f6197e342c6c131c18293a3fdf075e83d
Ozobywatel <ozobywatel@gmail.com>
2021-01-27 15:37:35 +0100

    App Recipe List do niepełne

---
+++ src/main/java/pl/coderslab/web/AppRecipeList.java
@@ -0,0 +1,28 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.RecipeDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/app/recipe/list")
+public class AppRecipeList extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+        RecipeDao recipeDao = new RecipeDao();
+        request.setAttribute("recipeList", recipeDao.findAll());
+        getServletContext().getRequestDispatcher("/appRecipeList.jsp")
+                .forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
