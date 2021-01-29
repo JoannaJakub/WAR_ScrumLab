143be42d197a1bf66d05c1f7b3cd97f7a788af2c
MikBi <Mikkolaj.bembas@gmail.com>
2021-01-29 23:01:49 +0000

    d

--- src/main/java/pl/coderslab/dao/PlanDao.java
+++ src/main/java/pl/coderslab/dao/PlanDao.java
@@ -122,7 +122,7 @@ public class PlanDao {
 
             boolean deleted = statement.execute();
             if (!deleted) {
-                throw new NotFoundException("Product not founs");
+                throw new NotFoundException("Product not foundd");
             }
         } catch (Exception e) {
             e.printStackTrace();
